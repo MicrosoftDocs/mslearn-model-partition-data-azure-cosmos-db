fee0f8302d3b5617aa65efbcf3529aaa5bfebc6f
Gary Hope <30275697+GaryHopeMS@users.noreply.github.com>
2021-05-05 12:54:37 -0700

    initial code

---
+++ init-sandbox.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Only for use for provisioning resources in an Azure subscription with one resource group
+
+resourceGroupName=$(az group list --query "[0].name" -o tsv)
+#resourceGroupName='mjbCosmicLabSandboxTest'
+deploymentName="CosmicLab-$RANDOM"
+
+az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file azuredeploy.json
+
+uri=$(az deployment group show \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --query "properties.outputs.uri.value" \
+    --output tsv)
+
+key=$(az deployment group show \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --query "properties.outputs.key.value" \
+    --output tsv)
+
+#key+=';'
+
+#delete appSettings.json
+rm -f "appSettings.json"
+
+appSettings=$(cat << EOF 
+{
+    "uri": "$uri", 
+    "key": "$key",
+    "gitdatapath" : "https://api.github.com/repos/GaryHopeMS/CosmicWorks/contents/modeling/data/"
+}
+EOF
+)
+echo "$appSettings" > "modeling-complete/appSettings.json"
+echo "$appSettings" > "modeling/appSettings.json"
+
+echo "Resource Group Name" $resourceGroupName
+echo "Deployment Name" $deploymentName
+echo "URI" $uri
+echo "Key" $key
+
+echo "Setup complete"
